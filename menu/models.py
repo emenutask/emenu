e90125f8ed49ea21c327f55e6bd5304f8111a1c6
Magda <magdalena@vocate.me>
2018-08-21 20:12:20 +0200

    create menu and dish models, enable them in admin

---
+++ menu/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.core import validators
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField('creation date', auto_now_add=True)
+    modified_at = models.DateTimeField('last modification date', auto_now=True)
+
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(
+        decimal_places=2,
+        max_digits=9,
+        validators=[validators.MinValueValidator(0)]
+    )
+    preparation_time = models.DurationField()
+    is_vegetarian = models.BooleanField()
+    created_at = models.DateTimeField('creation date', auto_now_add=True)
+    modified_at = models.DateTimeField('last modification date', auto_now=True)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'dishes'
+
+    def __str__(self):
+        return self.name
